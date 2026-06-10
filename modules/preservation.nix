df21f7115ea59a1280cf5d68ccf8caea4682f71d
pim <pim@NixBTW>
2026-06-11 00:03:31 +0200

    commit

--- modules/preservation.nix
+++ modules/preservation.nix
@@ -1,9 +1,15 @@
 {
   inputs,
   self,
+  config,
+  pkgs,
   ...
 }: {
-  inputs.flake.nixosModules.preservation = {pkgs, ...}: {
+  inputs.flake.nixosModules.preservation = {
+    config,
+    pkgs,
+    ...
+  }: {
     preservation = {
       enable = true;
       preserveAt."/persistent" = {
