82f1cca85f2faacbbb1edf3f01a5d19fc078de78
pim <pim@NixBTW>
2026-06-10 23:53:05 +0200

    removed initrd req

--- modules/preservation.nix
+++ modules/preservation.nix
@@ -4,17 +4,12 @@
   ...
 }: {
   inputs.flake.nixosModules.preservation = {pkgs, ...}: {
-    imports = [
-      inputs.preservation.nixosModules.preservation
-    ];
-
     preservation = {
       enable = true;
       preserveAt."/persistent" = {
         files = [
           {
             file = "/etc/machine-id";
-            inInitrd = true;
           }
         ];
         directories = [
