425b63273515ab2e4332dc08ead701f069c227bc
pim <pim@NixBTW>
2026-06-11 00:17:01 +0200

    last resort

--- modules/preservation.nix
+++ modules/preservation.nix
@@ -8,15 +8,17 @@
   inputs.flake.nixosModules.preservation = {
     config,
     pkgs,
+    lib,
     ...
   }: {
-    boot.initrd.systemd.enable = true;
-    preservation = {
+    config.boot.initrd.systemd.enable = true;
+    config.preservation = {
       enable = true;
       preserveAt."/persistent" = {
         files = [
           {
             file = "/etc/machine-id";
+            initrd = true;
           }
         ];
         directories = [
