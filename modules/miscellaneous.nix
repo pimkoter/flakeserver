6312fd27c9611f23b63ff47cb643686ea41a992a
pim <pim@NixBTW>
2026-06-11 20:52:20 +0200

    Added flakes

--- modules/miscellaneous.nix
+++ modules/miscellaneous.nix
@@ -8,5 +8,6 @@
     i18n.defaultLocale = "en_US.UTF-8";
     system.stateVersion = "25.11";
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    nix.settings.experimental-features = ["nix-command" "flakes"];
   };
 }
