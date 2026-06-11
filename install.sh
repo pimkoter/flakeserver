babc700d4602846a066cd4d2228d09fe9c46a729
pim <pim@NixBTW>
2026-06-11 18:13:36 +0200

    changed install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+set -e
 
-echo "Welcome to installer"
-read -p "Type machine name:" machine_name
+echo "Welcome to the installer"
+read -p "Type machine name: " machine_name
+
+if [[ -z "$machine_name" ]]; then
+    echo "Error: Machine name cannot be empty."
+    exit 1
+fi
 
 echo "Starting installation for machine $machine_name"
 
 sudo nix --extra-experimental-features "nix-command flakes" \
   run 'github:nix-community/disko/latest#disko-install' -- \
-  --flake .#$machine_name \
+  --flake ".#$machine_name" \
   --disk main /dev/sda
