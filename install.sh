1be1fe4cdbcec5203ea198a0de50a4eea8dfce64
pim <pim@NixBTW>
2026-06-11 18:07:26 +0200

    fixed typo

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Starting installation for machine $machine_name"
 
-sudo nix --extra-expirimental-featurs "nix-command flakes" \
+sudo nix --extra-experimental-featurs "nix-command flakes" \
 run 'github:nix-community/disko/latest#disko-install' -- \
 --flake .#$machine_name \
 --disk main /dev/sda
