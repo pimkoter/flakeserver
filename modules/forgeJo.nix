fa6eaedc0341cee94067739b0bc8ee1ba6eb2f23
pim <pim@NixBTW>
2026-06-13 00:57:50 +0200

    forgeJo error

--- modules/forgeJo.nix
+++ modules/forgeJo.nix
@@ -9,12 +9,6 @@
       database.type = "postgres";
       lfs.enable = true;
       settings = {
-        server = {
-          DOMAIN = "forgeJo.${self.settings.admin.domain}";
-          # You need to specify this to remove the port from URLs in the web UI.
-          ROOT_URL = "https://${srv.DOMAIN}/";
-          HTTP_PORT = 3000;
-        };
         service.DISABLE_REGISTRATION = false;
       };
     };
