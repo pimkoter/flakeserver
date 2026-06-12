32fa08f16ad5d6d8a1132b0d36a8960e083aa5e7
pim <pim@NixBTW>
2026-06-13 00:52:50 +0200

    Fixed logic error

--- flake.nix
+++ flake.nix
@@ -11,6 +11,9 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;}
-    (inputs.import-tree ./modules);
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        (inputs.import-tree ./modules)
+      ];
+    };
 }
