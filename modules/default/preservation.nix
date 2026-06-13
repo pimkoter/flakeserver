b5bc523a2399a4657aad796c346c12822f945474
pim <pim@NixBTW>
2026-06-13 12:03:17 +0200

    fixed rebuild issue

--- modules/default/preservation.nix
+++ modules/default/preservation.nix
@@ -5,15 +5,14 @@
 }: {
   flake.nixosModules.preservation = {
     imports = [inputs.preservation.nixosModules.default];
+    systemd.services.systemd-machine-id-commit.enable = false;
     preservation = {
       enable = true;
-
       preserveAt."/persistent" = {
         directories = [
           {
             directory = "/var/lib/nixos";
             inInitrd = true;
-            how = "symlink";
           }
         ];
 
