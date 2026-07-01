725f0fb6fc4d774b801da0347f612783353c1e64
pim <pim@koter>
2026-07-01 19:30:40 +0200

    fixed remaining modules

--- modules/hosts/delta.nix
+++ modules/hosts/delta.nix
@@ -7,6 +7,7 @@
 in {
   flake.nixosConfigurations.${name} = inputs.nixpkgs.lib.nixosSystem {
     modules = with self.nixosModules; [
+      {settings.hostName = name;}
       # Default modules
       autoUpgrade
       boot
