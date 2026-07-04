6546eae40677942ef86683037a33ed8a297176f6
pim <pim@koter>
2026-07-04 16:07:32 +0200

    fixed passwd?

--- modules/piHole.nix
+++ modules/piHole.nix
@@ -32,7 +32,7 @@
             active = true;
             port = lib.mkForce "80";
             domain = lib.mkForce "192.168.178.2";
-            api.pwhash = "$BALLOON-SHA256$v=1$s=1024,t=32$38d54e5d9fb97f6bdeb5887d3e990cd5626f0b4b2c6b57bcd43c044d5435135052a0e0d74b0522588869a46bfc2d4e7d7cb558b0ec3bf73b073ca85c4e77dbda";
+            api.pwhash = "$BALLOON-SHA256$v=1$s=1024,t=32$JmUiy69EGfJqy1/E9/o1Og==$KYi4l+qD/01Gj/J85mF9Ypg61eh2FylMYTVKqksDD/o=";
           };
 
           dns = {
