15303ee6113114b07f4b938ed7fcbdd30c71412c
pimkoter <pim@koter>
2026-07-18 23:43:22 +0200

    properly fixed zennotes server
    commit

--- modules/zenNotes.nix
+++ modules/zenNotes.nix
@@ -1,7 +1,37 @@
 {inputs, ...}: {
-  flake.nixosModules.zennotes = {pkgs, ...}: {
-    environment.systemPackages = [
-      inputs.zennotes.packages.${pkgs.stdenv.hostPlatform.system}.zennotes-server
-    ];
+  flake.nixosModules.zennotes = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    zennotes-pkg = inputs.zennotes.packages.${pkgs.stdenv.hostPlatform.system}.zennotes-server;
+    port = 8080;
+    vaultPath = "/home/${config.admin.name}/notes";
+  in {
+    users.users.zennotes = {
+      isSystemUser = true;
+      group = "zennotes";
+    };
+
+    users.groups.zennotes = {};
+
+    systemd.services.zennotes = {
+      description = "ZenNotes Server";
+      wantedBy = ["multi-user.target"];
+      after = ["network.target"];
+
+      serviceConfig = {
+        ExecStart = "${zennotes-pkg}/bin/zennotes-server";
+        Restart = "always";
+        User = "zennotes";
+        Group = "zennotes";
+        Environment = [
+          "PORT=${toString port}"
+          "VAULT_PATH=${vaultPath}"
+        ];
+        ReadWritePaths = [vaultPath];
+      };
+    };
   };
 }
