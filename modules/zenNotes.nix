61e60decb1e2f894abafdbf5bbe5ec1178f3583a
pimkoter <pim@koter>
2026-07-19 00:19:20 +0200

    fix

--- modules/zenNotes.nix
+++ modules/zenNotes.nix
@@ -15,7 +15,6 @@
     };
 
     users.groups.zennotes = {};
-
     systemd.services.zennotes = {
       description = "ZenNotes Server";
       wantedBy = ["multi-user.target"];
@@ -25,8 +24,12 @@
         ExecStart = "${zennotes-pkg}/bin/zennotes-server";
         Restart = "always";
         User = "zennotes";
-        Group = "zennotes";
+        Group = "zennotes"; # This is the primary group, it MUST exist
 
+        # Remove this line:
+        # SupplementaryGroups = [ config.admin.name ];
+
+        # Keep the namespaces open to ensure binary compatibility
         PrivateTmp = false;
         ProtectSystem = "off";
         ProtectHome = false;
@@ -37,7 +40,6 @@
           "VAULT_PATH=${vaultPath}"
         ];
         ReadWritePaths = [vaultPath];
-        SupplementaryGroups = [config.admin.name];
       };
     };
   };
