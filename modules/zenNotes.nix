aa6b4a1e524847bfe80a616547daf5b80a7717cb
pimkoter <pim@koter>
2026-07-21 12:17:45 +0200

    fixed zennotes
    updated zennotes version
    
    updated zennotes version

--- modules/zenNotes.nix
+++ modules/zenNotes.nix
@@ -1,47 +1,25 @@
 {inputs, ...}: {
   flake.nixosModules.zennotes = {
     config,
-    lib,
     pkgs,
     ...
-  }: let
-    zennotes-pkg = inputs.zennotes.packages.${pkgs.stdenv.hostPlatform.system}.zennotes-server;
-    port = 8080;
-    vaultPath = "/home/${config.admin.name}/notes";
-  in {
-    users.users.zennotes = {
-      isSystemUser = true;
-      group = "zennotes";
-    };
+  }: {
+    imports = [
+      inputs.zennotes.nixosModules.server
+    ];
 
-    users.groups.zennotes = {};
-    systemd.services.zennotes = {
-      description = "ZenNotes Server";
-      wantedBy = ["multi-user.target"];
-      after = ["network.target"];
+    # Configure the ZenNotes server
+    services.zennotes = {
+      enable = true;
+      port = 7878;
+      bindAddress = "0.0.0.0";
+      openFirewall = true;
+      dataDir = "/var/lib/zennotes";
+      vaultPath = "/var/lib/zennotes/vault";
 
-      serviceConfig = {
-        ExecStart = "${zennotes-pkg}/bin/zennotes-server";
-        User = "zennotes";
-        Group = "zennotes";
-
-        # 1. Remove BindPaths (Fixes 226/NAMESPACE)
-        # 2. Set HOME to your notes folder.
-        #    The binary will look for /home/pim/notes/ZenNotesVault
-        Environment = [
-          "PORT=${toString port}"
-          "HOME=/home/pim/notes"
-        ];
-
-        # 3. Disable Sandboxing explicitly
-        RestrictNamespaces = false;
-        SystemCallFilter = [];
-        PrivateTmp = false;
-        ProtectSystem = "no";
-        ProtectHome = "no";
-        PrivateDevices = false;
-
-        Restart = "always";
+      extraEnvironment = {
+        ZENNOTES_AUTH_TOKEN = "12345";
+        ZENNOTES_PERSIST_SESSIONS = "1";
       };
     };
   };
