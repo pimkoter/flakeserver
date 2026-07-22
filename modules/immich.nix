27f3ccbb7eb25fcff3dfe76688c7ecd7c769b2c8
pimkoter <pim@koter>
2026-07-22 22:45:46 +0200

    fixed immich
    fixed immich
    
    fixed immich

--- modules/immich.nix
+++ modules/immich.nix
@@ -1,21 +1,49 @@
 {
-  self,
-  inputs,
-  ...
-}: {
   flake.nixosModules.immich = {config, ...}: let
-    immichDir = "${config.disks.mntPoint}/foto";
+    baseDir = "${config.disks.mntPoint}/foto";
+    immichDataDir = "${baseDir}/immich-data";
+
+    # Immich's required integrity subdirectories
+    subDirs = [
+      "thumbs"
+      "upload"
+      "backups"
+      "library"
+      "profile"
+      "encoded-video"
+    ];
+
+    # Shell script to build structure & hidden marker files before service starts
+    initScript = ''
+      mkdir -p ${immichDataDir} ${baseDir}/library
+      ${builtins.concatStringsSep "\n" (map (dir: ''
+          mkdir -p ${immichDataDir}/${dir}
+          touch ${immichDataDir}/${dir}/.immich
+        '')
+        subDirs)}
+      chown -R immich:immich ${immichDataDir} ${baseDir}/library
+      chmod -R 750 ${immichDataDir}
+      chmod -R 755 ${baseDir}/library
+    '';
   in {
     services.immich = {
       enable = true;
       host = "0.0.0.0";
       openFirewall = true;
-      mediaLocation = immichDir;
+      mediaLocation = immichDataDir;
     };
-    systemd.services = {
-      immich-server.serviceConfig.ReadWritePaths = [immichDir];
-      immich-microservices.serviceConfig.ReadWritePaths = [immichDir];
-      immich-machine-learning.serviceConfig.ReadWritePaths = [immichDir];
+
+    # Automatically run the initial folder/file setup script inside systemd before Immich fires up
+    systemd.services.immich-server = {
+      preStart = initScript;
+      serviceConfig.ReadWritePaths = [baseDir];
     };
+
+    systemd.services.immich-microservices = {
+      preStart = initScript;
+      serviceConfig.ReadWritePaths = [baseDir];
+    };
+
+    systemd.services.immich-machine-learning.serviceConfig.ReadWritePaths = [baseDir];
   };
 }
