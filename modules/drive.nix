ed36f13d3f5f2f1f4c88eeafb9110fadde5d96f3
pimkoter <pim@koter>
2026-07-22 22:59:51 +0200

    added extra kernel params

--- modules/drive.nix
+++ modules/drive.nix
@@ -4,5 +4,10 @@
       device = config.disks.media;
       fsType = "ext4";
     };
+
+    boot.kernelParams = [
+      # Disable UAS for JMicron USB bridge (vendor 152d, product 0583)
+      "usb-storage.quirks=152d:0583:u"
+    ];
   };
 }
