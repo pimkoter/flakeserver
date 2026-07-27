2d633f07167339d3e9d3e66f93967a1b54fc9990
pimkoter <pim@koter>
2026-07-27 18:24:25 +0200

    Fixed immich with custom module

--- modules/immich.nix
+++ modules/immich.nix
@@ -1,49 +1,48 @@
 {
-  flake.nixosModules.immich = {config, ...}: let
-    baseDir = "${config.disks.mntPoint}/foto";
-    immichDataDir = "${baseDir}/immich-data";
+  flake.nixosModules.immich =
+    { config, lib, ... }:
+    let
+      baseDir = "${config.disks.mntPoint}/foto";
+      immichDataDir = "${baseDir}/immich-data";
 
-    # Immich's required integrity subdirectories
-    subDirs = [
-      "thumbs"
-      "upload"
-      "backups"
-      "library"
-      "profile"
-      "encoded-video"
-    ];
+      # Immich's required integrity subdirectories
+      subDirs = [
+        "thumbs"
+        "upload"
+        "backups"
+        "library"
+        "profile"
+        "encoded-video"
+      ];
 
-    # Shell script to build structure & hidden marker files before service starts
-    initScript = ''
-      mkdir -p ${immichDataDir} ${baseDir}/library
-      ${builtins.concatStringsSep "\n" (map (dir: ''
-          mkdir -p ${immichDataDir}/${dir}
-          touch ${immichDataDir}/${dir}/.immich
-        '')
-        subDirs)}
-      chown -R immich:immich ${immichDataDir} ${baseDir}/library
-      chmod -R 750 ${immichDataDir}
-      chmod -R 755 ${baseDir}/library
-    '';
-  in {
-    services.immich = {
-      enable = true;
-      host = "0.0.0.0";
-      openFirewall = true;
-      mediaLocation = immichDataDir;
+      # Shell script to build structure & hidden marker files before service starts
+      initScript = ''
+        mkdir -p ${immichDataDir} ${baseDir}/library
+        ${builtins.concatStringsSep "\n" (
+          map (dir: ''
+            mkdir -p ${immichDataDir}/${dir}
+            touch ${immichDataDir}/${dir}/.immich
+          '') subDirs
+        )}
+        chown -R immich:immich ${immichDataDir} ${baseDir}/library
+        chmod -R 750 ${immichDataDir}
+        chmod -R 755 ${baseDir}/library
+      '';
+    in
+    {
+      services.immich = {
+        enable = true;
+        host = "0.0.0.0";
+        openFirewall = true;
+        mediaLocation = immichDataDir;
+      };
+
+      # Automatically run the initial folder/file setup script inside systemd before Immich fires up
+      systemd.services.immich-server = {
+        preStart = lib.mkAfter initScript;
+        serviceConfig.ReadWritePaths = [ baseDir ];
+      };
+
+      systemd.services.immich-machine-learning.serviceConfig.ReadWritePaths = [ baseDir ];
     };
-
-    # Automatically run the initial folder/file setup script inside systemd before Immich fires up
-    systemd.services.immich-server = {
-      preStart = initScript;
-      serviceConfig.ReadWritePaths = [baseDir];
-    };
-
-    systemd.services.immich-microservices = {
-      preStart = initScript;
-      serviceConfig.ReadWritePaths = [baseDir];
-    };
-
-    systemd.services.immich-machine-learning.serviceConfig.ReadWritePaths = [baseDir];
-  };
 }
