16d513745576ab53fa0fe683607b82cf055ccb71
pimkoter <pim@koter>
2026-08-02 21:28:00 +0200

    fix(tailscale / syntax) added openFirewall and used nixfmt

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,43 +1,45 @@
 {
-  self,
-  inputs,
-  ...
-}: {
-  flake.nixosModules.networking = {
-    config,
-    lib,
-    ...
-  }: {
-    networking = {
-      hostName = config.hosts.${config.settings.hostName}.hostName;
-      useDHCP = false;
-      networkmanager.enable = false;
-      defaultGateway = config.admin.routerIp;
-      interfaces.ens18.ipv4.addresses = [
-        {
-          address = config.hosts.${config.settings.hostName}.ipAddr;
-          prefixLength = 24;
-        }
-      ];
-      firewall = {
-        enable = true;
-        trustedInterfaces = ["tailscale0"];
+  flake.nixosModules.networking =
+    {
+      config,
+      lib,
+      ...
+    }:
+    {
+      networking = {
+        hostName = config.hosts.${config.settings.hostName}.hostName;
+        useDHCP = false;
+        networkmanager.enable = false;
+        defaultGateway = config.admin.routerIp;
+        interfaces.ens18.ipv4.addresses = [
+          {
+            address = config.hosts.${config.settings.hostName}.ipAddr;
+            prefixLength = 24;
+          }
+        ];
+        firewall = {
+          enable = true;
+          trustedInterfaces = [ "tailscale0" ];
+        };
       };
-    };
 
-    services = {
-      openssh.enable = true;
-      tailscale = {
-        enable = true;
-        useRoutingFeatures = lib.mkDefault "client";
-        extraUpFlags = ["--ssh" "--operator=${config.admin.name}"];
+      services = {
+        openssh.enable = true;
+        tailscale = {
+          enable = true;
+          openFirewall = true;
+          useRoutingFeatures = lib.mkDefault "client";
+          extraUpFlags = [
+            "--ssh"
+            "--operator=${config.admin.name}"
+          ];
+        };
+        fail2ban = {
+          enable = true;
+          bantime = "10m";
+          bantime-increment.factor = "6";
+        };
+        resolved.enable = false;
       };
-      fail2ban = {
-        enable = true;
-        bantime = "10m";
-        bantime-increment.factor = "6";
-      };
-      resolved.enable = false;
     };
-  };
 }
