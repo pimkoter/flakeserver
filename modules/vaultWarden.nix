e5f68efaf304410fca27e2a323a64acdb0a23421
pimkoter <pim@koter>
2026-08-02 20:24:21 +0200

    fix(vaultwarden) Opened up ports

--- modules/vaultWarden.nix
+++ modules/vaultWarden.nix
@@ -1,25 +1,32 @@
 {
-  flake.nixosModules.vaultWarden = {
-    config,
-    pkgs,
-    ...
-  }: {
-    services.vaultwarden = {
-      enable = true;
-      dbBackend = "sqlite";
-      config = {
-        ROCKET_ADDRESS = "127.0.0.1";
-        ROCKET_PORT = 8222;
-        DOMAIN = "https://vault.example.org";
-        SIGNUPS_ALLOWED = true;
-        ADMIN_TOKEN = "";
-        LOG_FILE = "/var/lib/bitwarden_rs/access.log";
+  flake.nixosModules.vaultWarden =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      services.vaultwarden = {
+        enable = true;
+        dbBackend = "sqlite";
+        config = {
+          ROCKET_ADDRESS = "127.0.0.1";
+          ROCKET_PORT = 8222;
+          DOMAIN = "https://vault.example.org";
+          SIGNUPS_ALLOWED = true;
+          ADMIN_TOKEN = "";
+          LOG_FILE = "/var/lib/bitwarden_rs/access.log";
+        };
       };
-    };
 
-    # The CLI tool
-    environment.systemPackages = [
-      pkgs.vaultwarden
-    ];
-  };
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+
+      # The CLI tool
+      environment.systemPackages = [
+        pkgs.vaultwarden
+      ];
+    };
 }
