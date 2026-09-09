9e133892fab27df1aa7239055342b5fed1d4c502
pimkoter <pimkoter69@gmail.com>
2026-09-09 09:55:09 +0200

    feat: add shared nix store and persistence to blueprint

--- modules/hosts/blueprint/_hardware.nix
+++ modules/hosts/blueprint/_hardware.nix
@@ -12,5 +12,17 @@
         mac = "02:00:00:00:00:01";
       }
     ];
+    shares = [
+      {
+        tag = "ro-nix-store";
+        source = "/nix/store";
+        mountPoint = "/nix/store";
+      }
+      {
+        tag = "persistent";
+        source = "/var/lib/microvm/persistent/${config.settings.hostName}";
+        mountPoint = "/var/lib";
+      }
+    ];
   };
 }
